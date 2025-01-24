6e21bcbab746f0a52747880348381aeabf24f935
Arkadiusz Nowakowski <nowakowski.arkadiusz@gmail.com>
2025-01-24 11:57:30 +0100

    Preparation for splitting text and pattern

--- generate.py
+++ generate.py
@@ -11,19 +11,4 @@ random.seed(seed)
 TERMINALS = [chr(ord('a') + i) for i in range(sizeOfAlphabet)]
 OPERATORS = ['*', '', '|']
 
-def gen(size, starparent=False):
-    if size <= 1:
-        return random.choice(TERMINALS)
-    else:
-        if starparent:
-            op = random.choice(OPERATORS[1:])
-        else:
-            op = random.choice(OPERATORS)
-        if op == '*':
-            return '(' + gen(size - 1, True) + '*)'
-        size -= 1
-        r = random.randint(1, max(1, size - 1))
-        return '(' + gen(r) + op + gen(size - r) + ')'
-
 print(''.join(random.choices(TERMINALS, k=sizeOfText)))
-print(gen(sizeOfPattern, True))
