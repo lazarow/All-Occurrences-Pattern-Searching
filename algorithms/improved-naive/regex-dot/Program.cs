a8c2eebc1abf8e760f04d13236da5e586ab972d2
Arkadiusz Nowakowski <nowakowski.arkadiusz@gmail.com>
2025-02-07 22:06:31 +0100

    improve algorithm

--- algorithms/improved-naive/regex-dot/Program.cs
+++ algorithms/improved-naive/regex-dot/Program.cs
@@ -1,19 +1,5 @@
 ﻿using Fare;
 
-static int Match(Automaton aut, ref string text, int idx, int max_length)
-{
-    int length = 0;
-    int max_path = -1;
-    State? state = aut.Initial;
-    if (state.Accept) max_path = 0;
-    while (length < max_length && (state = state!.Step(text[idx++])) is not null)
-    {
-        ++length;
-        if (state.Accept) max_path = length;
-    }
-    return max_path;
-}
-
 if (args.Length == 0)
 {
     Console.WriteLine("Please provide a file name as the first argument.");
@@ -35,14 +21,14 @@ Automaton regex = new RegExp(pattern).ToAutomaton();
 int textLength = text.Length;
 
 for (int k = 0; k < textLength; k++) {
-    int m = Match(regex, ref text, k, textLength - k);
-    if (m > 0) {
-        Console.WriteLine($"{k}, {m}");
-        for (int j = 1; j < m; j++) {
-            int n = Match(regex, ref text, k, j);
-            if (n == j) {
-                Console.WriteLine($"{k}, {j}");
-            }
+
+    int length = 0;
+    int max_length = textLength - k;
+    State? state = regex.Initial;
+    while (length < max_length && (state = state!.Step(text[k + length++])) is not null)
+    {
+        if (state.Accept) {
+            Console.WriteLine($"{k}, {length}");
         }
     }
 }
